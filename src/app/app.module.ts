b309eb3831cfb0f6e79c5ff4ae0c7451f241df0e
Mohamed Hamou <MohamedHamou@gitlab.com>
2017-07-03 09:38:57 +0200

    nouvelle strucuture du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HistogramComponent } from './histogram/histogram.component';
+import { HistogramComponent } from '../components/histogram/histogram.component';
+import { HistogramDemoComponent } from './histogram-demo/histogram-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HistogramComponent,
+    HistogramDemoComponent,
   ],
   imports: [
     BrowserModule
