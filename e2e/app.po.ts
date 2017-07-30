4c29152f68317852c2cce6d9d6ce99909df6f2be
Matthieu Barbet @ Gisaia <matthieu.barbet@gisaia.com>
2017-07-30 15:14:43 +0200

    add CI

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 export class ArlasWebComponentsPage {
-  navigateTo() {
+  public navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  public getParagraphText() {
     return element(by.css('gisaia-root h1')).getText();
   }
 }
