787e0a45e61eda054eaa061ef1ddd7e1e89c8383
Mohamed Hamou <MohamedHamou@gitlab.com>
2017-08-23 11:24:08 +0200

    change actionlist input/output

--- src/components/results/utils/detailed-data-retriever.ts
+++ src/components/results/utils/detailed-data-retriever.ts
@@ -1,8 +1,12 @@
 
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
 
 export interface DetailedDataRetriever {
 
-  getData(identifier: string): Observable<{details: Map<string, string>, actions: Array<string>}>;
+  getData(identifier: string): Observable<{details: Map<string, string>,
+                                           actions: Array<{id: string, label: string,
+                                                          actionBus: Subject<{idFieldName: string, idValue: string}>}>}>;
 
 }
