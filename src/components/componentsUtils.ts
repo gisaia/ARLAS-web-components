ca06b706d3022ca3072fc22010ac1e33ac226886
MohamedHamou <mohamed.hamou@gisaia.com>
2018-07-03 19:07:53 +0200

    Fix #150 : Display the slected styles of all stylegroups

--- src/components/componentsUtils.ts
+++ src/components/componentsUtils.ts
@@ -1,3 +1,11 @@
 export interface ColorBuilder {
   getHexColorFromString(text: string): string;
 }
+
+export function getKeys(map): Array<string> {
+  return Array.from(map.keys());
+}
+
+export function getValues(map): Array<any> {
+  return Array.from(map.values());
+}
--- src/components/mapgl/mapgl.module.ts
+++ src/components/mapgl/mapgl.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSelectModule, MatOptionModule } from '@angular/material';
+import { MatSelectModule, MatRadioModule } from '@angular/material';
 import { MapglComponent } from './mapgl.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatSelectModule,
-    MatOptionModule
+    MatRadioModule
   ],
   declarations: [MapglComponent],
   exports: [MapglComponent]
