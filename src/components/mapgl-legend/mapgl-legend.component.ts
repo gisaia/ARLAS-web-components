3ad248fe9b145fa7b291354680ad03e4920d9f46
MohamedHamou <mohamed.hamou@gisaia.com>
2020-05-07 14:14:44 +0200

    Add width legend

--- src/components/mapgl-legend/mapgl-legend.component.ts
+++ src/components/mapgl-legend/mapgl-legend.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, Input, AfterViewInit, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, SimpleChanges, OnChanges, ElementRef, ViewChild } from '@angular/core';
 import { Subject } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
-import { ColorFormats } from 'tinycolor2';
+import { curveLinear, area } from 'd3-shape';
+import { scaleLinear, ScaleLinear } from 'd3-scale';
+import { axisLeft, axisBottom } from 'd3-axis';
+import { select } from 'd3-selection';
+import { HistogramData } from 'arlas-d3/histograms/utils/HistogramUtils';
+
 
 export const GET = 'get';
 export const MATCH = 'match';
@@ -16,14 +21,16 @@ export class MapglLegendComponent implements OnInit, AfterViewInit, OnChanges {
 
   @Input() public layer: mapboxgl.Layer;
   @Input() public legendContent: Subject<Map<string, {min: number, max: number}>>;
+  @ViewChild('width_svg', { read: ElementRef, static: false }) public widthSvg: ElementRef;
 
 
   public manualColors: Map<string, string> = new Map();
   public interpolatedColors: Array<string> = new Array();
 
   public colorType: PROPERTY_SELECTOR_SOURCE;
+  public widthType: PROPERTY_SELECTOR_SOURCE;
   public PROPERTY_SELECTOR_SOURCE = PROPERTY_SELECTOR_SOURCE;
-  constructor(public translate: TranslateService) { }
+  constructor(public translate: TranslateService, private el: ElementRef) { }
 
   public ngOnInit() {
   }
@@ -113,13 +120,56 @@ export class MapglLegendComponent implements OnInit, AfterViewInit, OnChanges {
             }
           }
         }
+
+
+        const lineWidth = p['line-width'];
+        if (typeof lineWidth === 'number') {
+
+        } else if (Array.isArray(lineWidth)) {
+          if (lineWidth.length >= 3) {
+            if (lineWidth[0] === INTERPOLATE) {
+              this.widthType = PROPERTY_SELECTOR_SOURCE.interpolated;
+              const data: Array<HistogramData> = new Array();
+              lineWidth.filter((w, i) => i >= 3).forEach((w, i) => {
+                if (i % 2 !== 0) {
+                  data.push({key: w, value: lineWidth[i + 1 + 3]});
+                }
+              });
+              lineWidthLegend(this.widthSvg.nativeElement, data, 300);
+            }
+          }
+        }
         break;
       }
     }
   }
 
 }
+export function lineWidthLegend(svgNode: SVGElement, data: Array<HistogramData>, width: number) {
+  const maxHeight = data[data.length - 1].value;
+  const xDomain: any = (scaleLinear()).range([0, width]);
+  const xDomainExtent = [data[0].key, data[data.length - 1].key];
+  xDomain.domain(xDomainExtent);
+  const yDomain: ScaleLinear<number, number> = scaleLinear().range([maxHeight, 0]);
+  yDomain.domain([0, maxHeight]);
+  const svg = select(svgNode);
+  const context = svg.append('g').attr('class', 'context');
+  // context.append('g')
+  //     .call(axisBottom(xDomain).tickSize(0));
+  // context.append('g')
+  //   .call(axisLeft(yDomain).tickSize(0).ticks(0));
+  const ar = area()
+      .curve(curveLinear)
+      .x((d: any) => xDomain(d.key))
+      .y0(maxHeight)
+      .y1((d: any) => yDomain(d.value));
 
+  context
+      .append('path')
+      .datum(data)
+      .attr('class', 'histogram__chart--unselected--area')
+      .attr('d', <any>ar);
+}
 export enum PROPERTY_SELECTOR_SOURCE {
   fix = 'Fix',
   provided = 'Provided',
