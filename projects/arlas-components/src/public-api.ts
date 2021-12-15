d2b0ceb24b61ce3401a0f439cedaed792ab8cd5a
MohamedHamouGisaia <mohamed.hamou@gisaia.com>
2021-12-15 17:38:26 +0100

    Upgrade typedoc version in doc generation script

--- projects/arlas-components/src/public-api.ts
+++ projects/arlas-components/src/public-api.ts
@@ -10,50 +10,56 @@ export { DonutComponent } from './lib/components/donut/donut.component';
 export { DonutModule } from './lib/components/donut/donut.module';
 export { HistogramComponent } from './lib/components/histogram/histogram.component';
 export { HistogramModule } from './lib/components/histogram/histogram.module';
-export { MapglImportComponent } from './lib/components/mapgl-import/mapgl-import.component';
+export { MapglImportComponent, MapglImportDialogComponent } from './lib/components/mapgl-import/mapgl-import.component';
 export { MapglImportModule } from './lib/components/mapgl-import/mapgl-import.module';
 export { MapglLayerIconComponent } from './lib/components/mapgl-layer-icon/mapgl-layer-icon.component';
 export { MapglLayerIconModule } from './lib/components/mapgl-layer-icon/mapgl-layer-icon.module';
-export { LayerIdToName } from './lib/components/mapgl-legend/layer-name.pipe';
-export { MapglLegendComponent } from './lib/components/mapgl-legend/mapgl-legend.component';
-export { MapglLegendModule } from './lib/components/mapgl-legend/mapgl-legend.module';
 export {
-  GeometrySelectModel, GeoQuery, MapglSettingsComponent, MapSettingsService, OperationSelectModel
-} from './lib/components/mapgl-settings/mapgl-settings.component';
-export { MapglSettingsModule } from './lib/components/mapgl-settings/mapgl-settings.module';
-export { CROSS_LAYER_PREFIX, IconConfig, MapglComponent, VisualisationSetConfig } from './lib/components/mapgl/mapgl.component';
-export { MapExtend } from './lib/components/mapgl/mapgl.component.util';
-export { MapglModule } from './lib/components/mapgl/mapgl.module';
-export {
-  ARLAS_VSET, BasemapStyle, MapLayers, SCROLLABLE_ARLAS_ID, BasemapStylesGroup, LayerEvents,
-  ExternalEventLayer, FillStroke, LayerMetadata, PaintColor, HOVER_LAYER_PREFIX, SELECT_LAYER_PREFIX,
+  MapLayers, BasemapStyle, ARLAS_VSET, LayerEvents, BasemapStylesGroup,
+  ExternalEventLayer, ExternalEvent, SCROLLABLE_ARLAS_ID, FillStroke, LayerMetadata, PaintColor, HOVER_LAYER_PREFIX, SELECT_LAYER_PREFIX,
   FILLSTROKE_LAYER_PREFIX, ARLAS_ID
 } from './lib/components/mapgl/model/mapLayers';
 export { MapSource } from './lib/components/mapgl/model/mapSource';
+export { MapExtend, LegendData, Legend, PROPERTY_SELECTOR_SOURCE } from './lib/components/mapgl/mapgl.component.util';
+export { PowerBar } from './lib/components/powerbars/model/powerbar';
+export { PageEnum } from './lib/components/results/utils/enumerations/pageEnum';
+export { ThumbnailFitEnum } from './lib/components/results/utils/enumerations/thumbnailFitEnum';
+export { CellBackgroundStyleEnum } from './lib/components/results/utils/enumerations/cellBackgroundStyleEnum';
+export { DetailedDataRetriever } from './lib/components/results/utils/detailed-data-retriever';
+export {
+  Action, ElementIdentifier, FieldsConfiguration, ResultListOptions, Attachment,
+  AdditionalInfo, Field, PageQuery
+} from './lib/components/results/utils/results.utils';
+export { Item } from './lib/components/results/model/item';
+export { Column } from './lib/components/results/model/column';
+export { MapglComponent, VisualisationSetConfig, IconConfig, OnMoveResult, CROSS_LAYER_PREFIX } from './lib/components/mapgl/mapgl.component';
+export { MapglModule } from './lib/components/mapgl/mapgl.module';
+export { ResultListComponent } from './lib/components/results/result-list/result-list.component';
+export { ResultItemComponent } from './lib/components/results/result-item/result-item.component';
+export { ResultDetailedItemComponent } from './lib/components/results/result-detailed-item/result-detailed-item.component';
+export { ResultsModule } from './lib/components/results/results.module';
+export { ColorGeneratorModule, ColorGeneratorModuleConfig } from './lib/services/color.generator.module';
+export { ArlasColorService } from './lib/services/color.generator.service';
+export { LayerIdToName } from './lib/components/mapgl-legend/layer-name.pipe';
+export { MapglLegendComponent } from './lib/components/mapgl-legend/mapgl-legend.component';
+export { MapglLegendModule } from './lib/components/mapgl-legend/mapgl-legend.module';
+export { MapglSettingsModule } from './lib/components/mapgl-settings/mapgl-settings.module';
 export { MetricComponent } from './lib/components/metric/metric.component';
 export { MetricModule } from './lib/components/metric/metric.module';
 export { PowerbarsComponent } from './lib/components/powerbars/powerbars.component';
 export { PowerbarsModule } from './lib/components/powerbars/powerbars.module';
-export { Column } from './lib/components/results/model/column';
-export { Item } from './lib/components/results/model/item';
+export {
+  MapglSettingsComponent, MapSettingsService, GeoQuery,
+  GeometrySelectModel, OperationSelectModel, MapglSettingsDialogComponent,
+} from './lib/components/mapgl-settings/mapgl-settings.component';
+export { WmtsLayerManagerModule } from './lib/components/wmts-layer-manager/wmts-layer-manager.module';
+export { WmtsLayerManagerComponent, GetTilesInfo, LayerParam, Dimension } from './lib/components/wmts-layer-manager/wmts-layer-manager.component';
+export { WmtsLayerManagertDialogComponent } from './lib/components/wmts-layer-manager/wmts-layer-manager.component';
 export { ResultDetailedGridComponent } from './lib/components/results/result-detailed-grid/result-detailed-grid.component';
-export { ResultDetailedItemComponent } from './lib/components/results/result-detailed-item/result-detailed-item.component';
 export { ResultScrollDirective } from './lib/components/results/result-directive/result-scroll.directive';
 export { ResultFilterComponent } from './lib/components/results/result-filter/result-filter.component';
 export { ResultGridTileComponent } from './lib/components/results/result-grid-tile/result-grid-tile.component';
-export { ResultItemComponent } from './lib/components/results/result-item/result-item.component';
-export { ResultListComponent } from './lib/components/results/result-list/result-list.component';
-export { ResultsModule } from './lib/components/results/results.module';
-export { DetailedDataRetriever } from './lib/components/results/utils/detailed-data-retriever';
-export { CellBackgroundStyleEnum } from './lib/components/results/utils/enumerations/cellBackgroundStyleEnum';
 export { ModeEnum } from './lib/components/results/utils/enumerations/modeEnum';
 export { SortEnum } from './lib/components/results/utils/enumerations/sortEnum';
-export { Action, ElementIdentifier, FieldsConfiguration, PageQuery, ResultListOptions } from './lib/components/results/utils/results.utils';
-export { WmtsLayerManagerComponent, WmtsLayerManagertDialogComponent } from './lib/components/wmts-layer-manager/wmts-layer-manager.component';
-export { WmtsLayerManagerModule } from './lib/components/wmts-layer-manager/wmts-layer-manager.module';
 export { FormatNumberModule } from './lib/pipes/format-number/format-number.module';
 export { FormatNumberPipe } from './lib/pipes/format-number/format-number.pipe';
-export { ColorGeneratorModule } from './lib/services/color.generator.module';
-export { ArlasColorService } from './lib/services/color.generator.service';
-
-
