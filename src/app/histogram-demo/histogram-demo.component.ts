1d2a23f80f5b7efe681c8f46a1fdcaa9d8b6fd1e
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2023-04-17 16:09:17 +0200

    build(deps): use latest arlas-d3 version

--- src/app/histogram-demo/histogram-demo.component.ts
+++ src/app/histogram-demo/histogram-demo.component.ts
@@ -19,6 +19,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { DataType, ChartType, SelectedOutputValues, SelectedInputValues, SwimlaneMode } from 'arlas-d3';
+import { SwimlaneData, SwimlaneStats } from 'arlas-d3/histograms/utils/HistogramUtils';
 
 
 @Component({
@@ -28,10 +29,9 @@ import { DataType, ChartType, SelectedOutputValues, SelectedInputValues, Swimlan
 })
 export class HistogramDemoComponent implements OnInit {
   public curvedTimelineData: Array<{key: Date | number; value: number; }>;
-  public barsHistogramData: Array<Array<{key: Date | number; value: number; }>>;
   public oneDimensionHistogramData: Array<Array<{key: Date | number; value: number; }>>;
   public defaultHistogramData: Array<{key: Date | number; value: number; }>;
-  public swimlaneHistogramData: Map<any, any>;
+  public swimlaneHistogramData: SwimlaneData;
   public dataType = DataType;
   public swimlaneMode = SwimlaneMode;
   public chartType = ChartType;
@@ -67,14 +67,14 @@ export class HistogramDemoComponent implements OnInit {
 
   private showData() {
     this.defaultHistogramData = [
-      {value: -400,   key: 0},
-      {value: -123,  key: 1},
-      {value: -333,  key: 2},
-      {value: -400,  key: 3},
-      {value: -400,  key: 4},
-      {value: -212,  key: 5},
-      {value: -111,  key: 6},
-      {value: -400,  key: 7},
+      {value: -400, key: 0},
+      {value: 123,  key: 1},
+      {value: -333, key: 2},
+      {value: -400, key: 3},
+      {value: -400, key: 4},
+      {value: -212, key: 5},
+      {value: -111, key: 6},
+      {value: -400, key: 7},
       {value: -400, key: 8},
       {value: -120, key: 9},
       {value: -120, key: 10},
@@ -83,6 +83,108 @@ export class HistogramDemoComponent implements OnInit {
       {value: -222, key: 13},
       {value: -120, key: 14},
     ];
+
+    this.curvedTimelineData = [
+      {value: 400, key: 1574208000000},
+      {value: 123, key: 1574209000000},
+      {value: 333, key: 1574210000000},
+      {value: 400, key: 1574211000000},
+      {value: 400, key: 1574212000000},
+      {value: 212, key: 1574213000000},
+      {value: 111, key: 1574214000000},
+      {value: 400, key: 1574215000000},
+      {value: 400, key: 1574216000000},
+      {value: 120, key: 1574217000000},
+      {value: 120, key: 1574218000000},
+      {value: 123, key: 1574219000000},
+      {value: 100, key: 1574220000000},
+      {value: 222, key: 1574221000000},
+      {value: 120, key: 1574222000000},
+    ];
+
+    const lanes = new Map<string, Array<{key: number; value: number;}>>();
+    lanes.set('Sweden', [
+      {key: 1574208000000, value: 46428},
+      {key: 1574211600000, value: 2278},
+      {key: 1574215200000, value: 3567},
+      {key: 1574218800000, value: 4716},
+      {key: 1574222400000, value: 5883},
+      {key: 1574226000000, value: 3529}
+    ]);
+    lanes.set('Denmark', [
+      {key: 1574208000000, value: 4958},
+      {key: 1574211600000, value: 95848},
+      {key: 1574215200000, value: 359},
+      {key: 1574218800000, value: 2954},
+      {key: 1574222400000, value: 394},
+      {key: 1574226000000, value: 38454}
+    ]);
+    lanes.set('Norway', [
+      {key: 1574208000000, value: 3564},
+      {key: 1574211600000, value: 4534},
+      {key: 1574215200000, value: 35334},
+      {key: 1574218800000, value: 3642},
+      {key: 1574222400000, value: 742},
+      {key: 1574226000000, value: 2563}
+    ]);
+
+    let sum = 0;
+    let max = -Infinity;
+    let min = Infinity;
+
+    lanes.forEach(v => {
+      v.forEach(val => {
+        sum += val.value;
+        if (max < val.value) {
+          max = val.value;
+        }
+        if ( min > val.value) {
+          min = val.value;
+        }
+      });
+    });
+
+    const columnStats = new Map();
+    const keys = lanes.get('Sweden').map(bucket => bucket.key);
+
+    keys.forEach((val, idx) => {
+      let sum = 0;
+      let max = -Infinity;
+      let min = Infinity;
+      lanes.forEach(lane => {
+        sum += lane[idx].value;
+        if (max < lane[idx].value) {
+          max = lane[idx].value;
+        }
+        if ( min > lane[idx].value) {
+          min = lane[idx].value;
+        }
+      });
+      columnStats.set(val, {
+        max: max,
+        min: min,
+        sum: sum
+      });
+    });
+
+    const stats: SwimlaneStats = {
+      bucketLength: 3600000,
+      columnStats: columnStats,
+      globalStats: {
+        min: min,
+        max: max,
+        sum: sum,
+        count: 18
+      },
+      maxBorder: 1574226000000,
+      minBorder: 1574208000000,
+      nbLanes: 3
+    };
+
+    this.swimlaneHistogramData = {
+      lanes: lanes,
+      stats: stats
+    };
   }
 
 
