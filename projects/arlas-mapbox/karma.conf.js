6bc9d25520e20ea05aeb976e0e520584d95dcda0
Merge angular migration to maplibre/mapbox refactor <mohamed.hamou@gisaia.com>
2024-12-20 15:47:01 +0100

    No Sonar for karma test

--- projects/arlas-mapbox/karma.conf.js
+++ projects/arlas-mapbox/karma.conf.js
@@ -20,6 +20,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+//NOSONAR
+
 module.exports = function (config) {
   config.set({
     basePath: '',
