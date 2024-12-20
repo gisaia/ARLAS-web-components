e69b0b3a5b7263214425c86a5585b26b78ba5544
Merge angular migration to maplibre/mapbox refactor <mohamed.hamou@gisaia.com>
2024-12-20 15:51:11 +0100

    exclude files

--- projects/arlas-mapbox/karma.conf.js
+++ projects/arlas-mapbox/karma.conf.js
@@ -20,7 +20,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-//NOSONAR
 
 module.exports = function (config) {
   config.set({
