b92db868da38e81c8f7dd0f23b2d888343fcd36a
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2025-05-21 10:04:55 +0200

    fix: export ArlasMapOption in arlas-map

--- projects/arlas-map/src/public-api.ts
+++ projects/arlas-map/src/public-api.ts
@@ -98,3 +98,4 @@ export { ArlasMapFrameworkService } from './lib/arlas-map-framework.service';
 export { GET, HEATMAP_DENSITY, IN, INTERPOLATE, MATCH, NOT_IN, OTHER } from './lib/map/model/filters';
 export { MapLayerMouseEvent, MapMouseEvent} from './lib/map/model/events';
 export { latLngToWKT } from './lib/map/tools';
+export { ArlasMapOption } from './lib/map/AbstractArlasMapGL';
