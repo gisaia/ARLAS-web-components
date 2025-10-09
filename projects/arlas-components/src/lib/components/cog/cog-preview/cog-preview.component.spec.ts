555e6620005f0cf903bc339a902314726408bd3a
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2025-10-09 14:05:59 +0200

    fix: karma tests

--- projects/arlas-components/src/lib/components/cog/cog-preview/cog-preview.component.spec.ts
+++ projects/arlas-components/src/lib/components/cog/cog-preview/cog-preview.component.spec.ts
@@ -1,6 +1,7 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CogPreviewComponent } from './cog-preview.component';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { CogPreviewComponent } from './cog-preview.component';
 
 describe('CogPreviewComponent', () => {
   let component: CogPreviewComponent;
@@ -8,9 +9,11 @@ describe('CogPreviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CogPreviewComponent,
+      imports: [
+        CogPreviewComponent,
         TranslateModule.forRoot({ loader: { provide: TranslateLoader, useClass: TranslateFakeLoader } })
-      ]
+      ],
+      providers: [provideHttpClient(withInterceptorsFromDi())]
     })
     .compileComponents();
 
--- projects/arlas-components/src/lib/tools/protect-image.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ProtectImageDirective } from './protect-image.directive';
-
-describe('ProtectImageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ProtectImageDirective();
-    expect(directive).toBeTruthy();
-  });
-});
