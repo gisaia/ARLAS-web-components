9afbd7a83e11ff95eefa87649125a54c70ef292a
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2026-03-24 18:09:39 +0100

    fix: add missing component import

--- projects/arlas-components/src/lib/components/cog/cog-modal/cog-modal.component.ts
+++ projects/arlas-components/src/lib/components/cog/cog-modal/cog-modal.component.ts
@@ -18,8 +18,9 @@
  */
 
 import { Component, inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { TranslateModule } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TranslatePipe } from '@ngx-translate/core';
 import { CogPreviewComponent } from '../cog-preview/cog-preview.component';
 import { VisualisationInterface } from '../model';
 
@@ -37,7 +38,8 @@ export interface CogVisualisationData {
   standalone: true,
   imports: [
     CogPreviewComponent,
-    TranslateModule
+    TranslatePipe,
+    MatButtonModule
   ],
   templateUrl: './cog-modal.component.html',
   styleUrl: './cog-modal.component.scss'
