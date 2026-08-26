fe9cf4e1fdf2c739e68f8ccfb1bb046a0f28a212
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2026-08-26 09:38:14 +0200

    fix: unsubscribe after tasks have been updated

--- projects/arlas-components/src/lib/components/results/model/itemComponent.ts
+++ projects/arlas-components/src/lib/components/results/model/itemComponent.ts
@@ -17,6 +17,7 @@
  * under the License.
  */
 
+import { first } from 'rxjs';
 import { Item } from '../model/item';
 import { DetailedDataRetriever } from '../utils/detailed-data-retriever';
 import { Action, Attachment } from '../utils/results.utils';
@@ -87,7 +88,7 @@ export class ItemComponent {
       item.tasks = new Map();
       const taskMap$ = detailedDataRetriever.getAllTasks(item.identifier);
       taskMap$.forEach((t$, service) => {
-        t$.subscribe(tasks => {
+        t$.pipe(first()).subscribe(tasks => {
           item.tasks.set(service, tasks);
         });
       });
